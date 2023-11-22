a72c80bd0d1574e6a6c092a9c95ab1681e9633d9
Harsh Deep <hdeep61034@gmail.com>
2023-11-22 11:25:25 +0530

    First Commit of Learning Typescript

---
+++ Lesson01/build/js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let username = 'Harsh';
+console.log(username);
+let a = 4;
+let b = '2';
+let c = 8;
---
+++ Lesson01/src/main.ts
@@ -0,0 +1,6 @@
+let username = 'Harsh';
+console.log(username);
+
+let a = 4;
+let b = '2';
+let c = 8;
