669af7f9430e6b253e409378c19f8811a68d2a85
Harsh Deep <hdeep61034@gmail.com>
2024-01-31 11:47:09 +0530

    learned Enums

--- node-app/index.js
+++ node-app/index.js
@@ -1,16 +1,77 @@
 "use strict";
-const user = {
-    firstName: "Harsh",
-    lastName: "Deep",
-    email: "hdeep61034@gmail.com",
-    age: 18,
-};
-function isLegal(user) {
-    if (user.age < 18) {
-        return false;
+// interface User {
+//   firstName: string;
+//   lastName: string;
+//   email: string;
+//   age: number;
+// }
+//
+// const user: User = {
+//   firstName: "Harsh",
+//   lastName: "Deep",
+//   email: "hdeep61034@gmail.com",
+//   age: 18,
+// };
+//
+// function isLegal(user: User): boolean {
+//   if (user.age < 18) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+//
+// console.log(isLegal(user));
+//
+//
+// interface UserArray {
+//   firstName: string;
+//   lastName: string;
+//   age: number;
+// }
+//
+// function userinfo(arr: UserArray[]) {
+//   for (let i = 0; i < arr.length; i++) {
+//     const user = arr[i];
+//     console.log(
+//       user.firstName + " " + user.lastName + " is " + user.age + " year old.",
+//     );
+//   }
+// }
+//
+// const users = [
+//   { firstName: "Harsh", lastName: "Deep", age: 18 },
+//   { firstName: "Harshit", lastName: "Pundir", age: 18 },
+//   { firstName: "Sudhanshu", lastName: "Sodiyal", age: 19 },
+// ];
+//
+// userinfo(users);
+//
+//
+var Directions;
+(function (Directions) {
+    Directions["up"] = "UP";
+    Directions["down"] = "Down";
+    Directions["left"] = "Left";
+    Directions["right"] = "Right";
+})(Directions || (Directions = {}));
+function gamelogic(keypressed) {
+    if (keypressed == Directions.up) {
+        console.log("Player Jumped");
     }
-    else {
-        return true;
+    else if (keypressed == Directions.down) {
+        console.log("Player Couched");
+    }
+    else if (keypressed == Directions.right) {
+        console.log("Player Moved Forwad!");
+    }
+    else if (keypressed == Directions.left) {
+        console.log("Player Moved Backwards!");
     }
 }
-console.log(isLegal(user));
+gamelogic(Directions.up);
+gamelogic(Directions.down);
+gamelogic(Directions.left);
+gamelogic(Directions.right);
+console.log(Directions.up);
+console.log(Directions.down);
--- node-app/index.ts
+++ node-app/index.ts
@@ -1,23 +1,94 @@
-interface User {
-  firstName: string;
-  lastName: string;
-  email: string;
-  age: number;
+// interface User {
+//   firstName: string;
+//   lastName: string;
+//   email: string;
+//   age: number;
+// }
+//
+// const user: User = {
+//   firstName: "Harsh",
+//   lastName: "Deep",
+//   email: "hdeep61034@gmail.com",
+//   age: 18,
+// };
+//
+// function isLegal(user: User): boolean {
+//   if (user.age < 18) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+//
+// console.log(isLegal(user));
+//
+//
+
+// interface UserArray {
+//   firstName: string;
+//   lastName: string;
+//   age: number;
+// }
+//
+// function userinfo(arr: UserArray[]) {
+//   for (let i = 0; i < arr.length; i++) {
+//     const user = arr[i];
+//     console.log(
+//       user.firstName + " " + user.lastName + " is " + user.age + " year old.",
+//     );
+//   }
+// }
+//
+// const users = [
+//   { firstName: "Harsh", lastName: "Deep", age: 18 },
+//   { firstName: "Harshit", lastName: "Pundir", age: 18 },
+//   { firstName: "Sudhanshu", lastName: "Sodiyal", age: 19 },
+// ];
+//
+// userinfo(users);
+//
+//
+
+// enum Directions {
+//   up = "UP",
+//   down = "Down",
+//   left = "Left",
+//   right = "Right",
+// }
+//
+// function gamelogic(keypressed: Directions) {
+//   if (keypressed == Directions.up) {
+//     console.log("Player Jumped");
+//   } else if (keypressed == Directions.down) {
+//     console.log("Player Couched");
+//   } else if (keypressed == Directions.right) {
+//     console.log("Player Moved Forwad!");
+//   } else if (keypressed == Directions.left) {
+//     console.log("Player Moved Backwards!");
+//   }
+// }
+//
+// gamelogic(Directions.up);
+// gamelogic(Directions.down);
+// gamelogic(Directions.left);
+// gamelogic(Directions.right);
+// console.log(Directions.up);
+// console.log(Directions.down);
+//
+
+
+
+enum ResponseStatus {
+    Success = 200,
+    NotFound = 404,
+    Error = 500
 }
 
-const user: User = {
-  firstName: "Harsh",
-  lastName: "Deep",
-  email: "hdeep61034@gmail.com",
-  age: 18,
-};
 
-function isLegal(user: User): boolean {
-  if (user.age < 18) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-console.log(isLegal(user));
+app.get("/', (req, res) => {
+    if (!req.query.userId) {
+			res.status(ResponseStatus.Error).json({})
+    }
+    // and so on...
+		res.status(ResponseStatus.Success).json({});
+})
